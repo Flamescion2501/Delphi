f4ca07e07b03faa949bb87d51d1c3d10ad25bf08
Artem Shmatko <artem_shmatko@protonmail.com>
2026-02-04 16:54:05 +0100

    Small fixes

--- train.py
+++ train.py
@@ -69,14 +69,15 @@ no_event_token_rate = 5
 
 # -----------------------------------------------------------------------------
 config_keys = [k for k, v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
-exec(open('configurator.py').read())  # overrides from command line or config file
+with open('configurator.py') as f:
+    exec(f.read())  # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys}  # will be useful for logging
 # -----------------------------------------------------------------------------
 
 os.makedirs(out_dir, exist_ok=True)
 torch.manual_seed(seed)
-torch.backends.cuda.matmul.fp32_precision = 'tf32'
-torch.backends.cudnn.conv.fp32_precision = 'tf32'
+torch.set_float32_matmul_precision('high')
+
 device_type = 'cuda' if 'cuda' in device else 'cpu'  # for later use in torch.autocast
 # note: float16 data type will automatically use a GradScaler
 ptdtype = {'float32': torch.float32, 'float64': torch.float64,
@@ -119,7 +120,7 @@ elif init_from == 'resume':
     print(f"Resuming training from {out_dir}")
     # resume training from a checkpoint.
     ckpt_path = os.path.join(out_dir, 'ckpt.pt')
-    checkpoint = torch.load(ckpt_path, map_location=device)
+    checkpoint = torch.load(ckpt_path, map_location=device, weights_only=False)
     checkpoint_model_args = checkpoint['model_args']
     # force these config attributes to be equal otherwise we can't even resume training
     # the rest of the attributes (e.g. dropout) can stay as desired from command line
@@ -142,7 +143,7 @@ elif init_from == 'resume':
 model.to(device)
 
 # initialize a GradScaler. If enabled=False scaler is a no-op
-scaler = torch.amp.GradScaler('cuda', enabled=(dtype == 'float16')) 
+scaler = torch.amp.GradScaler(device_type, enabled=(dtype == 'float16')) 
 
 # optimizer
 optimizer = model.configure_optimizers(weight_decay, learning_rate, (beta1, beta2), device_type)
@@ -210,7 +211,7 @@ local_iter_num = 0  # number of iterations in the lifetime of this process
 
 val_loss = None
 while True:
-
+    metrics = {"iter": iter_num}
     # determine and set the learning rate for this iteration
     lr = get_lr(iter_num) if decay_lr else learning_rate
     for param_group in optimizer.param_groups:
@@ -219,22 +220,17 @@ while True:
     # evaluate the loss on train/val sets and write checkpoints
     if iter_num % eval_interval == 0 and iter_num > 0:
         losses = estimate_loss()
-        if val_loss is None:
-            val_loss_unpooled = losses['val']
-        val_loss_unpooled = 0.1 * losses['val'] + 0.9 * val_loss_unpooled  # ie exponential decay
-        val_loss = val_loss_unpooled.sum().item()
-        print(f"step {iter_num}: train loss {losses['train'].sum().item():.4f}, val loss {losses['val'].sum().item():.4f} ({val_loss:.4f})")
-        if wandb_log:
-            wandb.log({
-                "iter": iter_num,
-                "train/agg_loss": losses['train'].sum().item(),
-                "val/loss":val_loss,
-                "val/loss_ce": val_loss_unpooled[0].item(),
-                "val/loss_dt": val_loss_unpooled[1].item()
-            })
+        val_loss = losses['val'].sum().item()
+        print(f"step {iter_num}: train loss {losses['train'].sum().item():.4f}, val loss {val_loss:.4f}")
 
-        if always_save_checkpoint or val_loss < best_val_loss:
-            best_val_loss = val_loss
+        metrics.update({
+            "train/agg_loss": losses['train'].sum().item(),
+            "val/loss":val_loss,
+            "val/loss_ce": losses['val'][0].item(),
+            "val/loss_dt": losses['val'][1].item()
+        })
+
+        if always_save_checkpoint or best_val_loss > val_loss:
             if iter_num > 0:
                 checkpoint = {
                     'model': model.state_dict(),
@@ -246,6 +242,9 @@ while True:
                 }
                 print(f"saving checkpoint to {out_dir}")
                 torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
+
+        if best_val_loss > val_loss:
+            best_val_loss = val_loss
 
         if iter_num % 10_000 == 0:
             checkpoint = {
@@ -269,7 +268,6 @@ while True:
             logits, loss, att = model(X, A, Y, B)
         # immediately async prefetch next batch while model is doing the forward pass on the GPU
         ix = torch.randint(len(train_p2i), (batch_size,))
-        # print(ix)
         X, A, Y, B = get_batch(ix, train_data, train_p2i, block_size=block_size, device=device,
                                padding='random', lifestyle_augmentations=True, select='left',
                                no_event_token_rate=no_event_token_rate, cut_batch=True)
@@ -295,14 +293,13 @@ while True:
         lossf = loss.item()  # loss as float. note: this is a CPU-GPU sync point
         print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms")
 
-        if wandb_log:
-            wandb.log({
-                "iter": iter_num,
-                "train/loss": loss,
-                "lr": lr,
-                "weights": wandb.Histogram(model.transformer.wte.weight.cpu().detach().numpy()),
-                "logits": wandb.Histogram(logits.cpu().detach().numpy()),
-            })
+        metrics.update({
+            "train/loss": lossf,
+            "lr": lr,
+        })
+
+    if wandb_log and (iter_num % log_interval == 0 or "val/loss" in metrics):
+        wandb.log(metrics)
 
     iter_num += 1
     local_iter_num += 1
